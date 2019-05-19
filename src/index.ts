cb7d0c6ecc658eb1264715afa504be1a6e0558b2
Lukas Lewandowski <llewandowski1990@gmail.com>
2019-05-19 21:28:48 +0200

    add paddle movement

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Paddle from './paddle'
+import InputHandler from './input'
 
 let canvas: any = document.getElementById('gameScreen');
 let ctx: any = canvas.getContext('2d');
@@ -11,8 +12,19 @@ ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 paddle.draw(ctx);
 
-// ctx.fillStyle = '#f00';
-// ctx.fillRect(10, 10, 200, 100);
+new InputHandler(paddle);
 
-// ctx.fillStyle = '#00f';
-// ctx.fillRect(300, 400, 50, 50);
+let lastTime = 0;
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
--- src/paddle.ts
+++ src/paddle.ts
@@ -1,19 +1,48 @@
 export default class Paddle {
+    public gameWidth: number;
+    
     public width: number;
     public height: number;
+    public maxSpeed: number;
+    public speed: number;
     public position: {[key: string]: number};
 
     constructor(gameWidth: number, gameHeight: number) {
-        this.width = 50;
-        this.height = 30;
-        
+        this.gameWidth = gameWidth;
+
+        this.width = 150;
+        this.height = 20;
+
+        this.maxSpeed = 7;
+        this.speed = 0;
+
         this.position = {
             x: gameWidth / 2 - this.width / 2,
             y: gameHeight - this.height - 10
         };
     };
 
+    moveLeft() {
+        this.speed = - this.maxSpeed;
+    }
+    moveRight() {
+        this.speed = this.maxSpeed;
+    }
+    stop() {
+        this.speed = 0;
+    }
+
     draw(ctx) {
+        ctx.fillStyle='#00f';
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     };
+
+    update(deltaTime) {
+        this.position.x += this.speed;
+    
+        if (this.position.x < 0) this.position.x = 0;
+    
+        if (this.position.x + this.width > this.gameWidth)
+          this.position.x = this.gameWidth - this.width;
+      }
 }
